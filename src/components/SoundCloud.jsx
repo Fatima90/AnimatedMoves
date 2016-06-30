f94a859bdd3a514571da880de19038ea62d09b83
Fatima Hammami <en.f.h@live.com>
2016-06-30 17:58:18 +0300

    First Project

--- src/components/SoundCloud.jsx
+++ src/components/SoundCloud.jsx
@@ -4,7 +4,7 @@ var SoundCloud = React.createClass({
 		<div className="widget">
 		<iframe id="soundcloud_widget" width="40%" height="166" scrolling="no"
 		 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/261067825&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-		 <button id="stream" className="buttons" >Play / Pause</button>
+		 <button id="stream" className="buttons" >Play</button>
 		</div>
 		)}
 });
